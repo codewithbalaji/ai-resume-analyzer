9d995630594e52989d95bc8c34062d25bc2793b1
Balaji D <balaji.d@calispec.ai>
2026-03-17 22:09:03 +0530

    feat: enhance ResumeCard component with image support and improve layout; add authentication handling in Auth component; implement Puter store for state management

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,10 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
 
 
 
@@ -13,6 +17,14 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
@@ -20,7 +32,7 @@ export default function Home() {
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
-    </section>
+   
 
     {resumes.length > 0 && (
       <div className="resumes-section">
@@ -31,5 +43,6 @@ export default function Home() {
         ))}
       </div>
     )}
+     </section>
   </main>
 }
